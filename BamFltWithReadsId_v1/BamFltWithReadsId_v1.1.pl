914eb54cf5921cb07558d826928b78e5c89888c3
XieZD <zhangdong_xie@foxmail.com>
2022-04-02 10:13:59 +0800

    Update BatchScriptForBam Version1.1

--- BamFltWithReadsId_v1/BamFltWithReadsId_v1.1.pl
+++ BamFltWithReadsId_v1/BamFltWithReadsId_v1.1.pl
@@ -10,17 +10,21 @@ use Parameter::BinList;
 
 my ($HelpFlag,$BinList,$BeginTime);
 my $ThisScriptName = basename $0;
-my ($OriBam,$FltBam,$IdList,$Samtools);
+my ($OriBam,$FltBam,$IdList,$SeqCheckFlag,$RCFlag,$Samtools);
 my $HelpInfo = <<USAGE;
 
  $ThisScriptName
  Auther: zhangdong_xie\@foxmail.com
 
   This script was used to extract info from bam with reads\' ID.
+  和上个版本相比，新增确认碱基序列功能。
 
  -i      ( Required ) Bam file;
  -o      ( Required ) Filterd bam file;
  -r      ( Required ) A file records the reads\' ID, one ID a line;
+ -b      ( Required ) If checking the base seq which will need the sequence;
+                      需要提供碱基序列。
+ -c      ( Required ) If the seq need to be reverse conplement;
 
  -bin    ( Optional ) List for searching of related bin or scripts; 
  -h      ( Optional ) Help infomation;
@@ -31,6 +35,8 @@ GetOptions(
 	'i=s' => \$OriBam,
 	'o=s' => \$FltBam,
 	'r=s' => \$IdList,
+	'b!' => \$SeqCheckFlag,
+	'c!' => \$RCFlag,
 	'bin:s' => \$BinList,
 	'h!' => \$HelpFlag
 ) or die $HelpInfo;
@@ -55,16 +61,29 @@ if(1)
 {
 	my %tmpHash = ();
 	my $TotalNum = 0;
-	open(ID,"cat $IdList |") or die $! unless($IdList =~ /\.gz$/);
-	open(ID,"zcat $IdList |") or die $! if($IdList =~ /\.gz$/);
+	my $CutString = "1";
+	$CutString = "1-" if($SeqCheckFlag);
+	open(ID,"cat $IdList | cut -f $CutString |") or die $! unless($IdList =~ /\.gz$/);
+	open(ID,"zcat $IdList | cut -f $CutString |") or die $! if($IdList =~ /\.gz$/);
 	while(my $Line = <ID>)
 	{
 		chomp $Line;
-		my @Cols = split /\t/, $Line;
-		if(!$tmpHash{$Cols[0]})
+		
+		if(!$tmpHash{$Line})
 		{
-			$tmpHash{$Cols[0]} = 1;
+			$tmpHash{$Line} = 1;
 			$TotalNum ++;
+		}
+		
+		# 是否需要反向互补;
+		if($RCFlag)
+		{
+			my @Cols = split /\t/, $Line;
+			$Cols[1] = uc($Cols[1]);
+			$Cols[1] = reverse($Cols[1]);
+			$Cols[1] =~ tr/ATCG/TAGC/;
+			$Line = join("\t",@Cols);
+			$tmpHash{$Line} = 1 if(!$tmpHash{$Line});
 		}
 	}
 	close ID;
@@ -81,7 +100,16 @@ if(1)
 		}
 		
 		my @Cols = split /\t/, $Line;
-		print FLT $Line if($tmpHash{$Cols[0]});
+		if($SeqCheckFlag)
+		{
+			next unless($Cols[9]);
+			my $tKey = join("\t",$Cols[0],$Cols[9]);
+			print FLT $Line if($tmpHash{$tKey});
+		}
+		else
+		{
+			print FLT $Line if($tmpHash{$Cols[0]});
+		}
 	}
 	close ORI;
 	close FLT;
